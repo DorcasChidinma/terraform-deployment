fa3870ead2b7c381385a1bd3ea963490ba027ade
Dorcas Agbakwuru <144885958+DorcasChidinma@users.noreply.github.com>
2023-12-02 15:56:08 +0100

    Update keyname.tf

--- keyname.tf
+++ keyname.tf
@@ -1,5 +1,5 @@
 # Defining Key Name for connection
 variable "keyname" {
-  default     = "terraform-project" 
-  description = "cba_keypair"
+  default     = "cba_keypair" 
+  description = "keypair"
 }
